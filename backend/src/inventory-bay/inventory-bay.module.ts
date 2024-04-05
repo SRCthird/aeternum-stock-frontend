b295b7a03afc04c875eee19a3ce5c99344f9434a
Stephen Chryn <SRCthird@gmail.com>
2024-04-05 19:42:42 -0400

    Import Database Module

--- backend/src/inventory-bay/inventory-bay.module.ts
+++ backend/src/inventory-bay/inventory-bay.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InventoryBayService } from './inventory-bay.service';
 import { InventoryBayController } from './inventory-bay.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [InventoryBayController],
   providers: [InventoryBayService],
 })
--- backend/src/inventory/inventory.module.ts
+++ backend/src/inventory/inventory.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [InventoryController],
   providers: [InventoryService],
 })
