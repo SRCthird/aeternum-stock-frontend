e729da62b1a4f683cf85d3233bec478efb1ddd49
Stephen Chryn <SRCthird@gmail.com>
2024-04-05 19:26:19 -0400

    Replace app with core

---
+++ backend/src/core.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { CoreController } from './core.controller';
+import { CoreService } from './core.service';
+
+import { DatabaseModule } from './database/database.module';
+import { InventoryModule } from './inventory/inventory.module';
+import { InventoryBayModule } from './inventory-bay/inventory-bay.module';
+import { LogModule } from './log/log.module';
+import { ProductModule } from './product/product.module';
+import { WarehouseModule } from './warehouse/warehouse.module';
+
+@Module({
+  imports: [
+    InventoryModule,
+    InventoryBayModule,
+    LogModule,
+    ProductModule,
+    WarehouseModule,
+    DatabaseModule,
+  ],
+  controllers: [CoreController],
+  providers: [CoreService],
+})
+export class CoreModule {}
