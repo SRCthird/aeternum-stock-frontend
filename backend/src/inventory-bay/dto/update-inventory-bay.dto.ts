62c00338d8f8507f2aa70da2e4f27d9f45cd151c
Stephen Chryn <SRCthird@gmail.com>
2024-04-05 19:31:32 -0400

    remove native dto's

--- backend/src/inventory-bay/dto/update-inventory-bay.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateInventoryBayDto } from './create-inventory-bay.dto';
-
-export class UpdateInventoryBayDto extends PartialType(CreateInventoryBayDto) {}
