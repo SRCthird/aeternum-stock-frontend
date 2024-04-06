3240ebb5e67488e1b235897c1d565161b2f35a8d
Stephen Chryn <SRCthird@gmail.com>
2024-04-05 22:05:40 -0400

    Start product managment screen

---
+++ src/screens/ProductManagementScreen.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+const ProductManagementScreen = () => {
+  return (
+    <View>
+      <Text>Product Management Screen</Text>
+    </View>
+  );
+}
+
+export default ProductManagementScreen;
+
