b18dfcf15a26fba2aae5d067a558962ea2fcbfc4
Stephen Chryn <SRCthird@gmail.com>
2024-04-05 22:05:47 -0400

    Create styles sheet

---
+++ src/styles/index.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  body: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    gap: 5,
+  },
+  action: {
+    backgroundColor: '#219ebc',
+    padding: 15,
+    marginBottom: 5,
+    width: '100%',
+    alignItems: 'center',
+  },
+  optionText: {
+    color: '#ffffff',
+    fontSize: 18,
+  },
+});
