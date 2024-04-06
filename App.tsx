d8f866e9ba8c6458c96e8068a3ec7b95c779387d
Stephen Chryn <SRCthird@gmail.com>
2024-04-05 22:06:11 -0400

    Wrap with paper provider

--- App.tsx
+++ App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import HomePage from 'src/HomePage';
+import ActionScreen from '@screens/ActionScreen';
+import ProductManagementScreen from '@screens/ProductManagementScreen';
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomePage} />
-        {/* Add other screens here */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Action Screen" component={ActionScreen} />
+          <Stack.Screen name="Product Management" component={ProductManagementScreen} />
+          {/* Add other screens here */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
 
