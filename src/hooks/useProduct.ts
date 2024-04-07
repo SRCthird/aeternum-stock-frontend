e66e4ec5d7612c5142f4ee8b8a5ebd309b4543f8
Stephen Chryn <SRCthird@gmail.com>
2024-04-06 22:46:41 -0400

    Create useProduct webhook

---
+++ src/hooks/useProduct.ts
@@ -0,0 +1,41 @@
+import api from '@src';
+import { CanceledError } from 'axios';
+import { useEffect, useState } from 'react'
+
+type Product = {
+  id: number;
+  lotnumber: string;
+  description: string;
+}
+
+const useProduct = ({ id }: { id?: number }) => {
+  const [result, setResult] = useState<Product[]>([]);
+  const [error, setError] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (id === 0) return;
+    setLoading(true);
+    const controller = new AbortController();
+    const endpoint = id ? `/api/product/${id}` : '/api/product';
+    api.get(endpoint, { signal: controller.signal })
+      .then(response => {
+        setResult(response.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+    return () => controller.abort();
+  }, []);
+
+  if (typeof result === 'string') {
+    setResult([]);
+  }
+
+  return { result, error, isLoading };
+}
+
+export default useProduct;
