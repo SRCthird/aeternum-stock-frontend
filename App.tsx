769d04386170168b29f1f139ece017be2b08c7c7
Stephen Chryn <SRCthird@gmail.com>
2024-04-08 22:54:54 -0400

    Move stack into Home

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import ActionScreen from '@screens/ActionScreen';
-import ProductManagementScreen from '@screens/ProductManagementScreen';
 import { Provider as PaperProvider } from 'react-native-paper';
-
-const Stack = createStackNavigator();
+import Home from '@screens/Home';
 
 const App = () => {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Action Screen" component={ActionScreen} />
-          <Stack.Screen name="Product Management" component={ProductManagementScreen} />
-          {/* Add other screens here */}
-        </Stack.Navigator>
+        <Home />
       </NavigationContainer>
     </PaperProvider>
   );
