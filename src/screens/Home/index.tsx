2c402416973c03f6c8b89240e10e412656fe3f78
Stephen Chryn <SRCthird@gmail.com>
2024-04-10 14:41:56 -0400

    Add product lot to home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,12 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import ActionScreen from '@screens/Home/ActionScreen';
+import Home from '@screens/Home/Home';
 import Product from '@screens/Product';
+import ProductLot from '@screens/ProductLot';
 
 export type RootStackParamList = {
   Actions: undefined;
   Product: undefined;
+  ProductLot: undefined;
 }
 
 const Stack = createStackNavigator();
@@ -14,8 +16,9 @@ const index = () => {
     <Stack.Navigator screenOptions={{
       headerShown: false
     }}>
-      <Stack.Screen name="Actions" component={ActionScreen} />
+      <Stack.Screen name="Actions" component={Home} />
       <Stack.Screen name="Product" component={Product} />
+      <Stack.Screen name="ProductLot" component={ProductLot} />
     </Stack.Navigator>
   );
 }
