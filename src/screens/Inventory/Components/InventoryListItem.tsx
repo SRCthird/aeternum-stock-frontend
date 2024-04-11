fa4b092834c6aa915fb01e72bcd02a7fe9310bc9
Stephen Chryn <SRCthird@gmail.com>
2024-04-11 10:22:27 -0400

    Adjust the look of Inventory list item

--- src/screens/Inventory/Components/InventoryListItem.tsx
+++ src/screens/Inventory/Components/InventoryListItem.tsx
@@ -1,7 +1,9 @@
 import { Inventory } from '../Hooks/useInventory';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { mode } from "@utils/types";
+import { useEffect, useState } from 'react';
+import moment from 'moment';
 
 type Props = {
   listItem: Inventory;
@@ -9,31 +11,73 @@ type Props = {
   setItem: (item: Inventory) => void;
 }
 const InventoryListItem = ({ listItem, setMode, setItem }: Props) => {
+  const [edited, setEdited] = useState(false);
+
+  const styles = StyleSheet.create({
+    body: {
+        padding: 15,
+        borderWidth: 1,
+        borderColor: '#ccc',
+        borderRadius: 10,
+        backgroundColor: '#f9f9f9',
+        marginVertical: 5,
+        marginHorizontal: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 3,
+        elevation: 2,
+    },
+    container: {
+      flexDirection: 'row', 
+      justifyContent: 'space-between',
+    },
+    label: {
+      flex: 1,
+      textAlign: 'center',
+    },
+    text: {
+      flex: 2,
+      textAlign: 'center',
+    },
+  });
+
+  useEffect(() => {
+    if (listItem.updatedBy !== listItem.createdBy) {
+      setEdited(true);
+    }
+  }, []);
+
+  const formatDate = (date?: Date) => {
+    const dt = moment(date);
+    return dt.format('MM/DD/YYYY HH:mm');
+  }
 
   return (
     <TouchableOpacity
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'stretch',
-        padding: 10,
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderColor: '#ccc',
-        margin: 10
-      }}
+      style={styles.body}
       onPress={() => {
         setItem(listItem);
         setMode('edit');
       }}
     >
-      <Text style={{ flex: 1 }}>{listItem.lotNumber}</Text>
-      <Text style={{ flex: 1 }}>{listItem.location}</Text>
-      <Text style={{ flex: 1 }}>{listItem.quantity}</Text>
-      <Text style={{ flex: 1 }}>{listItem.createdAt.toString()}</Text>
-      <Text style={{ flex: 1 }}>{listItem.createdBy}</Text>
-      <Text style={{ flex: 1 }}>{listItem.updatedAt?.toString()}</Text>
-      <Text style={{ flex: 1 }}>{listItem.updatedBy}</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>{listItem.lotNumber}</Text>
+        <Text style={styles.text}>{listItem.location}</Text>
+        <Text style={styles.text}>{listItem.quantity}</Text>
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.label}>Created: </Text>
+        <Text style={styles.text}>{formatDate(listItem.createdAt)}</Text>
+        <Text style={styles.text}>{listItem.createdBy}</Text>
+      </View>
+      {edited && (
+        <View style={styles.container}>
+          <Text style={styles.label}>Updated: </Text>
+          <Text style={styles.text}>{formatDate(listItem.updatedAt)}</Text>
+          <Text style={styles.text}>{listItem.updatedBy}</Text>
+        </View>
+      )}
     </TouchableOpacity>
   );
 }
