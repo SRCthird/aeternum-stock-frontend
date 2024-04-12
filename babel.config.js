dbb8ee9f53e4aa0dd463275108783b5e395ef446
Stephen Chryn <SRCthird@gmail.com>
2024-04-12 14:26:24 -0400

    Add context to alias

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = function(api) {
             '@screens': './src/screens',
             "@styles": "./src/styles",
             "@utils": "./src/utils",
+            "@context": "./src/context",
           }
         }
       ]
