df78739e63de35af03305fe4b8c989de91cf048a
Stephen Chryn <SRCthird@gmail.com>
2024-04-12 20:31:15 -0400

    Consolidate the use effect and context into one file

--- src/context/AccountContext.ts
+++ src/context/AccountContext.ts
@@ -1,6 +1,15 @@
-import { User } from '@src/screens/Login/Hooks/useUser';
+import { User } from '@src/screens/Authenticate/Hooks/useUser';
 import {createContext} from 'react';
+import { useContext } from 'react';
 
 const AccountContext = createContext<User | null>(null);
+
+export const useAccount = () => {
+  const account = useContext(AccountContext);
+  if (account === null) {
+    throw new Error('useAccount must be used within a UserAccount.Provider');
+  }
+  return account;
+}
 
 export default AccountContext;
