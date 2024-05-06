2b4183684f8df6f5208785c7c3f9f1d69662f06f
Stephen Chryn <SRCthird@gmail.com>
2024-05-06 14:47:43 -0400

    Migrate Warehouse.service

--- backend/src/warehouse/warehouse.service.ts
+++ backend/src/warehouse/warehouse.service.ts
@@ -1,59 +1,81 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { Warehouse } from '@prisma/client';
+import { Kysely } from 'kysely';
 import { DatabaseService } from 'src/database/database.service';
+import { Database } from 'src/database/types';
 
 @Injectable()
 export class WarehouseService {
+  private db: Kysely<Database>;
 
-  constructor(readonly databaseService: DatabaseService) { }
+  constructor(readonly databaseService: DatabaseService) {
+    this.db = databaseService.getKyselyInstance();
+  }
 
-  async create(createDto: Prisma.WarehouseCreateInput) {
-    return this.databaseService.warehouse.create({ data: createDto });
+  async create(createDto: Warehouse) {
+    const { insertId } = await this.db
+      .insertInto('Warehouse')
+      //.values(createDto)
+      .onDuplicateKeyUpdate(createDto)
+      .executeTakeFirstOrThrow();
+
+    return await this.findOne(Number(insertId));
   }
 
   async list() {
-    const warehouses = await this.databaseService.warehouse.findMany({
-      select: {
-        name: true,
-      }
-    });
-
+    const warehouses = await this.db
+      .selectFrom('Warehouse')
+      .select('name')
+      .execute();
     return warehouses.map(warehouse => warehouse.name);
   }
 
-  async findAll(
-    name?: string,
-  ) {
-    const query: Prisma.WarehouseFindManyArgs = {
-      where: {
-        name: {
-          contains: name,
-        },
-      },
-    };
-    return this.databaseService.warehouse.findMany(query);
+  async findAll(name?: string) {
+    try {
+      const warehouses = await this.db.selectFrom('Warehouse')
+        .where('name', '=', name)
+        .execute();
+      return warehouses;
+    } catch (error) {
+      throw new HttpException('Error fetching warehouses: ' + error.message, 500);
+    }
   }
 
   async findOne(id: number) {
-    return this.databaseService.warehouse.findUnique({ where: { id } });
+    try {
+      const warehouse = await this.db.selectFrom('Warehouse')
+        .where('id', '=', id)
+        .executeTakeFirst();
+      return warehouse[0];
+    } catch (error) {
+      throw new HttpException('Warehouse not found', 404);
+    }
   }
 
-  async update(id: number, updateDto: Prisma.WarehouseUpdateInput) {
-    return this.databaseService.warehouse.update({ where: { id }, data: updateDto });
+  async update(id: number, updateDto: Warehouse) {
+    await this.db.updateTable('Warehouse')
+      .set(updateDto)
+      .where('id', '=', id)
+      .execute();
+
+    return await this.findOne(id);
   }
 
   async remove(id: number) {
-    const dependency = await this.databaseService.inventoryBay.findMany({
-      where: {
-        warehouse: {
-          id: id
-        }
-      },
-    })
+    const warehouse = await this.findOne(id);
+
+    const dependency = await this.db.selectFrom('InventoryBay')
+      .where('warehouseName', '=', warehouse.name)
+      .execute();
 
     if (dependency.length > 0) {
       throw new HttpException('Warehouse has dependencies', HttpStatus.NOT_ACCEPTABLE);
     }
-    return this.databaseService.warehouse.delete({ where: { id } });
+
+    await this.db.deleteFrom('Warehouse')
+      .where('id', '=', id)
+      .execute();
+
+    return warehouse;
   }
 }
