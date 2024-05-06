9423992894c60ed9ca44b3f5979a0781e0afbe57
Stephen Chryn <SRCthird@gmail.com>
2024-05-06 14:28:49 -0400

    Modify User endpoint for query generator

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,40 +1,65 @@
-import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { HttpException, Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { Kysely } from 'kysely';
 import { DatabaseService } from 'src/database/database.service';
+import { Database } from 'src/database/types';
 
 @Injectable()
 export class UserService {
+  private db: Kysely<Database>;
 
-  constructor(readonly databaseService: DatabaseService) { }
-
-  create(createDto: Prisma.UserCreateInput) {
-    return this.databaseService.user.create({ data: createDto });
+  constructor(readonly databaseService: DatabaseService) {
+    this.db = this.databaseService.getKyselyInstance();
   }
 
-  findAll(email: string) {
-    const query: Prisma.UserFindManyArgs = { where: 
-      { email } 
-    };
+  async create(createDto: User) {
+    const { insertId } = await this.db
+      .insertInto('User')
+      //.values(createDto)
+      .onDuplicateKeyUpdate(createDto)
+      .executeTakeFirstOrThrow();
 
-    return this.databaseService.user.findMany(query);
+    return await this.findOne(Number(insertId));
   }
 
-  findOne(id: number) {
-    return this.databaseService.user.findUnique({
-      where: { id },
-    });
+  async findAll(email: string) {
+    try {
+      const users = await this.db.selectFrom('User')
+        .where('email', '=', email)
+        .execute();
+      return users;
+    } catch (error) {
+      throw new HttpException('Error fetching users: ' + error.message, 500);
+    }
   }
 
-  update(id: number, updateDto: Prisma.UserUpdateInput) {
-    return this.databaseService.user.update({
-      where: { id },
-      data: updateDto,
-    });
+  async findOne(id: number) {
+    try {
+      const user = await this.db.selectFrom('User')
+        .where('id', '=', id)
+        .executeTakeFirst();
+      return user;
+    } catch (error) {
+      throw new HttpException('User not found', 404);
+    }
   }
 
-  remove(id: number) {
-    return this.databaseService.user.delete({
-      where: { id },
-    });
+  async update(id: number, updateDto: User) {
+    await this.db.updateTable('User')
+      .set(updateDto)
+      .where('id', '=', id)
+      .execute();
+
+    return await this.findOne(id);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+
+    this.db.deleteFrom('User')
+      .where('id', '=', id)
+      .execute();
+
+    return user;
   }
 }
