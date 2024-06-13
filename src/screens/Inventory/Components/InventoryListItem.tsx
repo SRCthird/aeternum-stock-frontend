dd7b111ef548f6e15face6fb092dcbd46e9afc2a
Stephen Chryn <SRCthird@gmail.com>
2024-06-13 10:39:35 -0400

    Update Inventory Card for a cleaner look

--- src/screens/Inventory/Components/InventoryListItem.tsx
+++ src/screens/Inventory/Components/InventoryListItem.tsx
@@ -1,9 +1,10 @@
 import { Inventory } from '../Hooks/useInventory';
-import { StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import { mode } from "@utils/types";
 import { useEffect, useState } from 'react';
 import moment from 'moment';
+import { Caption, Card, Paragraph } from 'react-native-paper';
+import styles from '@src/utils/styles';
+import { api } from '@src/screens/Authenticate/Login';
 
 type Props = {
   listItem: Inventory;
@@ -12,40 +13,17 @@ type Props = {
 }
 const InventoryListItem = ({ listItem, setMode, setItem }: Props) => {
   const [edited, setEdited] = useState(false);
-
-  const styles = StyleSheet.create({
-    body: {
-        padding: 15,
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 10,
-        backgroundColor: '#f9f9f9',
-        marginVertical: 5,
-        marginHorizontal: 10,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-        elevation: 2,
-    },
-    container: {
-      flexDirection: 'row', 
-      justifyContent: 'space-between',
-    },
-    label: {
-      flex: 1,
-      textAlign: 'center',
-    },
-    text: {
-      flex: 2,
-      textAlign: 'center',
-    },
-  });
+  const [product, setProduct] = useState("")
 
   useEffect(() => {
     if (formatDate(listItem.updated_at) !== "Invalid date") {
       setEdited(true);
     }
+    api.get(`/api/product-lot/?lot_number=${listItem.lot_number}`)
+      .then((response) => {
+        console.log(response.data);
+        setProduct(response.data[0].product_name);
+      })
   }, []);
 
   const formatDate = (date?: Date) => {
@@ -54,31 +32,25 @@ const InventoryListItem = ({ listItem, setMode, setItem }: Props) => {
   }
 
   return (
-    <TouchableOpacity
-      style={styles.body}
+    <Card
+      style={styles.card_body}
       onPress={() => {
         setItem(listItem);
         setMode('edit');
       }}
     >
-      <View style={styles.container}>
-        <Text style={styles.text}>{listItem.lot_number}</Text>
-        <Text style={styles.text}>{listItem.location}</Text>
-        <Text style={styles.text}>{listItem.quantity}</Text>
-      </View>
-      <View style={styles.container}>
-        <Text style={styles.label}>Created: </Text>
-        <Text style={styles.text}>{formatDate(listItem.created_at)}</Text>
-        <Text style={styles.text}>{listItem.created_by}</Text>
-      </View>
-      {edited && (
-        <View style={styles.container}>
-          <Text style={styles.label}>Updated: </Text>
-          <Text style={styles.text}>{formatDate(listItem.updated_at)}</Text>
-          <Text style={styles.text}>{listItem.updated_by}</Text>
-        </View>
-      )}
-    </TouchableOpacity>
+      <Card.Title
+        titleStyle={styles.header_title}
+        title={listItem.lot_number}
+        subtitleStyle={styles.header_title}
+        subtitle={product}
+      />
+      <Card.Content>
+        <Paragraph style={styles.header_title}>Quantity: {listItem.quantity}</Paragraph>
+        <Caption style={styles.header_title}>{edited ? "Created at: " : "Updated at: "} {formatDate(listItem.updated_at ?? listItem.created_at)}</Caption>
+        <Caption style={styles.header_title}>{edited ? "Created by: " : "Updated by: "} {listItem.updated_by ?? listItem.created_by}</Caption>
+      </Card.Content>
+    </Card>
   );
 }
 
