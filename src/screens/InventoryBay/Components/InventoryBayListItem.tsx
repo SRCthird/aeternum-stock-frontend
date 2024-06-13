c72cacacdc6fa6ea70ca65099d99272cf4cf02f0
Stephen Chryn <SRCthird@gmail.com>
2024-06-13 10:50:58 -0400

    Update InventoryBay item for a cleaner look

--- src/screens/InventoryBay/Components/InventoryBayListItem.tsx
+++ src/screens/InventoryBay/Components/InventoryBayListItem.tsx
@@ -1,7 +1,7 @@
 import { InventoryBay } from '../Hooks/useInventoryBay';
-import { Text } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import { mode } from "@utils/types";
+import styles from '@src/utils/styles';
+import { Card, Paragraph } from 'react-native-paper';
 
 type Props = {
   listItem: InventoryBay;
@@ -11,27 +11,23 @@ type Props = {
 const InventoryBayListItem = ({ listItem, setMode, setItem }: Props) => {
 
   return (
-    <TouchableOpacity
-      style={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'stretch',
-        padding: 10,
-        borderWidth: 1,
-        borderStyle: 'solid',
-        borderColor: '#ccc',
-        margin: 10
-      }}
+    <Card
+      style={styles.card_body}
       onPress={() => {
         setItem(listItem);
         setMode('edit');
       }}
     >
-      <Text style={{ flex: 1 }}>{listItem.name}</Text>
-      <Text style={{ flex: 1 }}>{listItem.warehouse_name}</Text>
-      <Text style={{ flex: 1 }}>{listItem.max_unique_lots}</Text>
-    </TouchableOpacity>
+      <Card.Title
+        titleStyle={styles.header_title}
+        title={listItem.name}
+        subtitleStyle={styles.header_title}
+        subtitle={listItem.warehouse_name}
+      />
+      <Card.Content>
+        <Paragraph style={styles.header_title}>{listItem.max_unique_lots}</Paragraph>
+      </Card.Content>
+    </Card>
   );
 }
 
